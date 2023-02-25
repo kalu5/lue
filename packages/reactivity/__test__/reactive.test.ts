e1e4a8aa151a7ce935990a0e39ec987f5565283c
kalu5 <451660550@qq.com>
2023-02-25 17:25:59 +0800

    feat:cleaup

--- packages/reactivity/__test__/reactive.test.ts
+++ packages/reactivity/__test__/reactive.test.ts
@@ -1,5 +1,5 @@
 import { reactive, effect } from '../lib/index'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 
 describe('reactive', () => {
   it ('bese reactive', () => {
@@ -14,5 +14,27 @@ describe('reactive', () => {
     expect(result).toBe('LUE.')
     obj.name = 'LUE.....'
     expect(result).toBe('LUE.....')
+  })
+
+  it('cleanup', () => {
+    const data = { name: 'lue', ok: true }
+    let result = ''
+    let obj = reactive(data)
+    const fn = vi.fn(() => {
+      result = obj.ok ? obj.name : '-'
+    })
+    effect(fn)
+    expect(result).toBe('lue')
+    // 执行次数
+    expect(fn).toBeCalledTimes(1)
+    obj.name = 'LUE.'
+    expect(result).toBe('LUE.')
+    expect(fn).toBeCalledTimes(2)
+    obj.ok = false
+    expect(result).toBe('-')
+    expect(fn).toBeCalledTimes(3)
+    obj.name = 'none'
+    expect(fn).toBeCalledTimes(3)
+
   })
 })
--- packages/reactivity/lib/effect.ts
+++ packages/reactivity/lib/effect.ts
@@ -1,10 +1,6 @@
 // 存储当前的副作用函数
 export let activeEffect = null
 
-export function getActiveEffect() {
-  return activeEffect
-}
-
 // 存储代理对象的副作用函数
 /**
  * target: {
@@ -26,6 +22,8 @@ export function track(target, key) {
     depsMap.set(key, deps)
   }
   deps.add(activeEffect)
+  // 将所有依赖添加到deps中，执行副作用函数之前，清除
+  activeEffect.deps.push(deps)
 }
 
 export function trigger(target, key) {
@@ -33,11 +31,32 @@ export function trigger(target, key) {
   if (!depsMap) return 
   const deps = depsMap.get(key)
   if (!deps) return
-  deps.forEach(fn => fn())
+  // Set结构， delete add 会死循环，用新Set处理
+  const depsTorun = new Set(deps)
+  depsTorun.forEach(fn => fn())
+  
 }
 
 export function effect (fn) {
-  activeEffect = fn
-  // 触发get函数
-  fn()
+  const effectFn = () => {
+    try {
+      cleanup(effectFn)
+      activeEffect = effectFn
+      fn()
+    } finally {
+      activeEffect = null
+    }
+  }
+  // 当前副作用的所有依赖
+  effectFn.deps = []
+  effectFn()
+  return effectFn
+}
+
+function cleanup(effectFn) {
+  for (let i = 0 ; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+  effectFn.deps.length = 0
 }
